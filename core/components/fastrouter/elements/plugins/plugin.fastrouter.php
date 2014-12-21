8c1de725e9d71d2ecb2e3da1f311422ee7902608
vanchelo <npobolka@ya.ru>
2014-12-22 01:06:33 +0200

    Update plugin. Stop recursion when using sendErrorPage method

--- core/components/fastrouter/elements/plugins/plugin.fastrouter.php
+++ core/components/fastrouter/elements/plugins/plugin.fastrouter.php
@@ -3,7 +3,7 @@ require_once MODX_CORE_PATH . 'components/fastrouter/fastrouter.class.php';
 
 $router = new FastRouter($modx);
 
-if ($modx->event->name == 'OnPageNotFound') {
+if ($modx->event->name == 'OnPageNotFound' && !isset($modx->event->params['stop'])) {
     $router->dispatch();
 } else if ($modx->event->name == 'OnChunkSave' && $chunk->name == 'fastrouter') {
     $router->clearCache();
